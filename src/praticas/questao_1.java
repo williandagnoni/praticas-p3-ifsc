1368cdbee368162b1c19a33454dd37fb105970a0
willian donega <willqqwwqq@gmail.com>
2024-04-03 17:19:54 -0300

    questao 2

--- src/praticas/questao_1.java
+++
@@ -1,6 +0,0 @@
-package praticas;
-
-public class questao_1 {
-
-	System.out.println
-}
---
+++ src/questao_2/Pessoa.java
@@ -0,0 +1,41 @@
+package questao_2;
+
+import java.util.Scanner;
+
+public class Pessoa {
+	    public String nome;
+	    public long cpf;
+	    public String dataNascimento;
+
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+
+	        Pessoa[] pessoas = new Pessoa[3]; 
+
+	       
+	        for (int i = 0; i < pessoas.length; i++) {
+	        	Pessoa pessoa = new Pessoa();
+
+	            System.out.println("Digite o nome da pessoa " + (i+1) + ":");
+	            pessoa.nome = scan.nextLine();
+
+	            System.out.println("Digite o CPF da pessoa " + (i+1) + ":");
+	            pessoa.cpf = scan.nextLong();
+	            scan.nextLine(); 
+	            System.out.println("Digite a data de nascimento da pessoa " + (i+1) + " (no formato dd/mm/aaaa):");
+	            pessoa.dataNascimento = scan.nextLine();
+
+	            pessoas[i] = pessoa; 
+	        }
+
+	        scan.close();
+
+	        // Exibindo os dados
+	        for (int i = 0; i < pessoas.length; i++) {
+	            System.out.println("\nDados da pessoa " + (i+1) + ":");
+	            System.out.println("Nome: " + pessoas[i].nome);
+	            System.out.println("CPF: " + pessoas[i].cpf);
+	            System.out.println("Data de Nascimento: " + pessoas[i].dataNascimento);
+	        }
+	    }
+	}
